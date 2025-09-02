f4157a6c5880b31dd650974ce49ff7a01767a72a
Corujas <ari.nas4bpm@gmail.com>
2025-09-02 13:57:56 -0300

    Update produtividade.py

--- produtividade.py
+++ produtividade.py
@@ -306,7 +306,7 @@ st.divider()
 st.subheader("RESUMO DOS DADOS")
 st.write(f"Total de registros encontrados: {len(df)}")
 #st.write(f"Quantidade: {sum(df['QTDE'])}")
-st.write(f"Total de Pontos: {sum(df['PONTOS'])}")
+st.write(f"Total de Pontos: {df['PONTOS'].sum(skipna=True)}")
 
 
 st.divider()
@@ -409,10 +409,11 @@ st.plotly_chart(fig, use_container_width=True)
 st.divider()
 st.subheader("RESUMO DOS DADOS")
 st.write(f"Total de registros encontrados: {len(df)}")
-st.write(f"Total de Pontos: {sum(df['PONTOS'])}")
+st.write(f"Total de Pontos: {df['PONTOS'].sum(skipna=True)}")
 
 
 st.divider()
+
 
 
 
